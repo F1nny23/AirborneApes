3387d7d27ebd5951f49ad0b3e1aab05804b269bb
F1nny23 <82478247+F1nny23@users.noreply.github.com>
2021-06-01 10:47:50 +1200

    leevels

--- player.gd
+++ player.gd
@@ -1,3 +1,4 @@
+extends Spatial
 
 var turn_right = false
 var turn_left = false 
@@ -14,9 +15,22 @@ var rot_y = 0.0
 var rot_z = 0.0
 
 var velocity = Vector3()
-onready var movement_guide = $MovementGuide
 
 var camera_pos_1 = "Upper"
 var camera_pos_2 = "Right"
 
 var lower_camera_height_default = -10.0                     
+
+func _physics_process(delta):
+	
+	if going_up:
+		rot_x += 0.3
+	elif rot_x > 5:
+		rot_x -= 0.7
+	if going_down:
+		rot_x -= 0.3
+	elif rot_x < -5:
+		rot_x += 0.7
+		
+	rot_x = clamp(rot_x ,-25.0,25)
+
