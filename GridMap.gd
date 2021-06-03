b80dff2bd17649193e815f6a6ac9fe878a209392
F1nny23 <82478247+F1nny23@users.noreply.github.com>
2021-06-03 13:23:23 +1200

    grid map

---
+++ GridMap.gd
@@ -0,0 +1,12 @@
+extends GridMap
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var tunnel_length = 200
+
+func _ready():
+	for i in range(tunnel_length):
+		set_cell_item(0,-i,0,randi()%5)
