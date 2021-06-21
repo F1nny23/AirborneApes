d060b80e6d14289278a754acaf7ca6d174da8dcf
F1nny23 <82478247+F1nny23@users.noreply.github.com>
2021-06-21 12:22:43 +1200

    Started title screen

---
+++ AirBorneApes2/title_screen.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_Start_pressed():
+	get_tree() .change_scene("res://GameLevel.tscn")
+
+
+func _on_Settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	pass # Replace with function body.
